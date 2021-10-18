195917b6b742d0f86b8e9033f99b6d186302972e
jakew7 <jakewhite97@hotmail.com>
2021-10-18 15:06:34 -0400

    updated hello

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,24 @@
+let votesArray = [0, 0, 0, 0];
+
+
 exports.handler = async event => {
-    const subject = event.queryStringParameters.name || 'World'
+  const v = event.queryStringParameters.vote
+  if (vote.value.value === "a") {
+    votesArray[0] += 1;
+  } else if (v === "b") {
+    votesArray[1] += 1;
+  } else if (v === "c") {
+    votesArray[2] += 1;
+  } else if (v === "d") {
+    votesArray[3] += 1;
+  } else if (v === "r") {
+    votesArray = [0, 0, 0, 0];
+  }
+  console.log(JSON.stringify(votesArray));
+
     return {
       statusCode: 200,
-      body: `Hello ${subject}!`,
+      header: { 'Access-Control-Allow-Origin' : '*'},
+      body: JSON.stringify(votesArray),
     }
   }
